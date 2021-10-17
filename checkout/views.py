9b2c4840fa0a8d4b2e3a18ef4e0abb6b28b079e4
Yusuffrih <yusuffrih@hotmail.com>
2021-10-17 16:41:34 +0000

    Completed webhook handler

--- checkout/views.py
+++ checkout/views.py
@@ -11,7 +11,6 @@ from bag.context import bag_contents
 import stripe
 import json
 
-
 @require_POST
 def cache_checkout_data(request):
     try:
@@ -49,7 +48,11 @@ def checkout(request):
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
@@ -78,8 +81,7 @@ def checkout(request):
                     return redirect(reverse('view_bag'))
 
             request.session['save_info'] = 'save-info' in request.POST
-            return redirect(reverse(
-                'checkout_success', args=[order.order_number]))
+            return redirect(reverse('checkout_success', args=[order.order_number]))
         else:
             messages.error(request, 'There was an error with your form. \
                 Please double check your information.')
